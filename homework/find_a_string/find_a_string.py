4f16819b52362a59ea2781a5a9e38119c92e17bc
mgyulaaaaa <gyula.mate@stud.uni-corvinus.hu>
2024-10-18 23:10:48 +0200

    elso

--- homework/find_a_string/find_a_string.py
+++ homework/find_a_string/find_a_string.py
@@ -1,3 +1,17 @@
 def find_a_string(original_str: str, substr: str) -> int:
-    """Write your code here"""
-    pass
+    hossz=len(substr)
+    if hossz==0:
+        return 0 
+    else:
+        teljeshossz=len(original_str)
+        valasz=0
+        for i in range(teljeshossz-hossz+1):
+            if substr==original_str[i:i+hossz]:
+                valasz+=1
+        return valasz
+    
+
+
+nagy = "abab"
+kicsi = "a"
+print(find_a_string(nagy,kicsi))
